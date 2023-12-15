8c12a0d74a5b7d1993492e20956eb6ecd09df003
InvisiBug <matthewjkavanagh@gmail.com>
2023-12-15 15:00:34 +0000

    didnt work

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/health', (req, res) => {
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  console.log("Hey matt, you have this working great job")
+  console.log("That didnt work")
   res.send(`Hello ${name}!`);
 });
