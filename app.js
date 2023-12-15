0f3ceba7b3d3429a2cfd9ff0e9568f9a4bb646c8
InvisiBug <matthewjkavanagh@gmail.com>
2023-12-15 17:11:48 +0000

    Added prettier

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.listen(port);
 console.log(`App running at http://localhost:${port}`);
-console.log()
-const name = process.env.NAME || 'World';
-console.log("Booper")
+console.log();
+const name = process.env.NAME || "World";
+console.log("Booper");
 console.log(`Current version is: ${name}!`);
+console.log("This better work");
 
-app.get('/health', (req, res) => {
-  res.send('OK');
+app.get("/health", (req, res) => {
+  res.send("OK");
   res.status(200);
 });
 
-app.get('/', (req, res) => {
-});
+app.get("/", (req, res) => {});
