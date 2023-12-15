a1e9cd851d68db31586ec1ba8f85cab6b77de14f
InvisiBug <matthewjkavanagh@gmail.com>
2023-12-15 17:15:18 +0000

    added dev branch

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const name = process.env.NAME || "World";
 console.log("Booper");
 console.log(`Current version is: ${name}!`);
 console.log("This better work");
+console.log("This was on the dev branch, but should now be live");
 
 app.get("/health", (req, res) => {
   res.send("OK");
